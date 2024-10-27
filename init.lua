6dd35b1abf11a544145957fc137b03a23bc52c75
varocode <alvaroacevedo83@gmail.com>
2024-10-27 14:40:53 -0400

    version 1.0

---
+++ init.lua
@@ -0,0 +1,15 @@
+if vim.loader then
+	vim.loader.enable()
+end
+
+_G.dd = function(...)
+	require("util.debug").dump(...)
+end
+vim.print = _G.dd
+
+vim.opt.wrap = true -- Aquí activas el wrap
+
+require("config.lazy")
+
+-- Cargar la configuración de DAP
+require("plugins.dap")
